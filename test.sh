b155bf9105fcf85cf3555019984b7737391b0ba4
Eva Li <china.cons@yandex.ru>
2022-09-10 12:55:29 +0300

    Add function

--- test.sh
+++ test.sh
@@ -3,4 +3,30 @@ echo -n "Таков Путь. Введите свое имя"
 read name
 echo "Привет, $name. Таков Путь!"
 
+example() {
+
+   echo "Сколько будет 2+2?"
+
+   read answer
+
+   if [[ $answer == 4 ]]; then
+
+      echo "Yeap"
+
+   else
+
+      echo "Nope"
+
+      echo
+
+      example
+
+   fi
+
+}
+
+echo "Пример рекурсивной функции"
+
+example
+
 
